a3d3c08e76c7e22ff000ba6a007b70ab9d83f559
Ahmedshams1 <ahmedsama111997@gmail.com>
2024-05-08 23:50:09 +0300

    add_assets_styles_asset_generator

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/views/Dash_Board_View.dart';
+
+void main() {
+  runApp(const ResponsiveDashBoard());
+}
+
+class ResponsiveDashBoard extends StatelessWidget {
+  const ResponsiveDashBoard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DashBoardView(),
+    );
+  }
+}
---
+++ lib/utils/app_images.dart
@@ -0,0 +1,56 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAvatar1
+  /// assets/images/avatar1.svg
+  static const String imagesAvatar1 = "assets/images/avatar1.svg";
+
+  /// Assets for imagesAvater2
+  /// assets/images/avater2.svg
+  static const String imagesAvater2 = "assets/images/avater2.svg";
+
+  /// Assets for imagesBalance
+  /// assets/images/balance.svg
+  static const String imagesBalance = "assets/images/balance.svg";
+
+  /// Assets for imagesCardbackgraund
+  /// assets/images/cardbackgraund.svg
+  static const String imagesCardbackgraund = "assets/images/cardbackgraund.svg";
+
+  /// Assets for imagesDashBoard
+  /// assets/images/DashBoard.svg
+  static const String imagesDashBoard = "assets/images/DashBoard.svg";
+
+  /// Assets for imagesExpenses
+  /// assets/images/expenses.svg
+  static const String imagesExpenses = "assets/images/expenses.svg";
+
+  /// Assets for imagesIncome
+  /// assets/images/income.svg
+  static const String imagesIncome = "assets/images/income.svg";
+
+  /// Assets for imagesLogoutaccount
+  /// assets/images/logoutaccount.svg
+  static const String imagesLogoutaccount = "assets/images/logoutaccount.svg";
+
+  /// Assets for imagesMyinvestments
+  /// assets/images/myinvestments.svg
+  static const String imagesMyinvestments = "assets/images/myinvestments.svg";
+
+  /// Assets for imagesMytransaction
+  /// assets/images/mytransaction.svg
+  static const String imagesMytransaction = "assets/images/mytransaction.svg";
+
+  /// Assets for imagesSettingsystem
+  /// assets/images/settingsystem.svg
+  static const String imagesSettingsystem = "assets/images/settingsystem.svg";
+
+  /// Assets for imagesStatistics
+  /// assets/images/statistics.svg
+  static const String imagesStatistics = "assets/images/statistics.svg";
+
+  /// Assets for imagesWalletaccount
+  /// assets/images/walletaccount.svg
+  static const String imagesWalletaccount = "assets/images/walletaccount.svg";
+}
+
